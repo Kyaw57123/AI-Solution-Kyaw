6d606e844877c4f2036e8830ade244f8400f6a0b
Kyaw57123 <kyawtungusto@gmail.com>
2025-03-10 16:56:34 +0630

    Updated README comments

---
+++ Webpages/firebaseConfig.js
@@ -0,0 +1,11 @@
+// firebaseConfig.js
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyD64oPEDpzdrv3c64Yy2sxtRRDAdq6-0vY",
+    authDomain: "aisolution-dfe38.firebaseapp.com",
+    projectId: "aisolution-dfe38",
+    storageBucket: "aisolution-dfe38.firebasestorage.app",
+    messagingSenderId: "971485624298",
+    appId: "1:971485624298:web:0ee76b576d1ac2a3b4e493",
+    measurementId: "G-NJJFG1BNJN"
+};
